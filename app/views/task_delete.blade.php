6d17a308bc779df4e1736647fe1a5aa50c7fe21a
Teri Chu <terichu@Teris-MacBook-Pro.local>
2014-12-03 21:40:02 -0500

    still working on task_delete

--- app/views/task_delete.blade.php
+++ app/views/task_delete.blade.php
@@ -7,10 +7,10 @@
 @section('head')
 @stop
 
-@section('content')
+@section('content')         
 
     <h1>Delete task called {{ $task->task_name }}?</h1>
-{{ Form::open(array('action' => array('TaskController@postDelete', $user->id))) }}
+{{ Form::open(array('action' => array('TaskController@postDelete', Auth::user()->id))) }}
 {{ Form::submit('Yes', array('class' => 'btn')) }}
 {{ Form::close() }}
 <p><a href="/list">No, go back to my task list.</a></p>@stop
