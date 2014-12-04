2e0e06f31463f614a37b162fdea9255f8bb0a528
Teri Chu <terichu@Teris-MacBook-Pro.local>
2014-12-03 22:50:47 -0500

    task delete still in progress

--- app/views/task_delete.blade.php
+++ app/views/task_delete.blade.php
@@ -10,7 +10,7 @@
 @section('content')         
 
     <h1>Delete task called {{ $task->task_name }}?</h1>
-{{ Form::open(array('action' => array('TaskController@postDelete', Auth::user()->id))) }}
+{{ Form::open(array('action' => array('TaskController@postDelete', $task->id))) }}
 {{ Form::submit('Yes', array('class' => 'btn')) }}
 {{ Form::close() }}
 <p><a href="/list">No, go back to my task list.</a></p>@stop
